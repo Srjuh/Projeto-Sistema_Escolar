40456ba1f1b84c410e07b544404c5466773f727f
Sergio <srg.a.neto@gmail.com>
2025-12-08 01:36:32 -0300

    Simplificação das rotas públicas

--- routes/siteRoutes.js
+++ routes/siteRoutes.js
@@ -1,49 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-// Página inicial
-router.get('/', (req, res) => {
-    res.render('pages/site/home');
-});
-
-// Página de login
-router.get('/login', (req, res) => {
-    res.render('pages/site/login');
-});
-
-// Página de cadastro
-router.get('/matricula', (req, res) => {
-    res.render('pages/site/matricula');
-});
-
-// Página sobre nós
-router.get('/sobre', (req, res) => {
-    res.render('pages/site/sobre');
-});
-
-// Página de contato
-router.get('/contato', (req, res) => {
-    res.render('pages/site/contato');
-});
-
-// Página de time
-router.get('/time', (req, res) => {
-    res.render('pages/site/time');
-});
-
-// Página de feedback
-router.get('/feedbacks', (req, res) => {
-    res.render('pages/site/feedbacks');
-});
-
-// Página de confirmação
-router.get('/confirmacao', (req, res) => {
-    res.render('pages/site/confirmacao');
-});
-
-// Página de cursos
-router.get('/cursos', (req, res) => {
-    res.render('pages/site/cursos');
-});
+// Páginas públicas
+router.get('/', (req, res) => res.render('pages/site/home'));
+router.get('/login', (req, res) => res.render('pages/site/login'));
+router.get('/matricula', (req, res) => res.render('pages/site/matricula'));
+router.get('/sobre', (req, res) => res.render('pages/site/sobre'));
+router.get('/contato', (req, res) => res.render('pages/site/contato'));
+router.get('/time', (req, res) => res.render('pages/site/time'));
+router.get('/feedbacks', (req, res) => res.render('pages/site/feedbacks'));
+router.get('/confirmacao', (req, res) => res.render('pages/site/confirmacao'));
+router.get('/cursos', (req, res) => res.render('pages/site/cursos'));
 
 module.exports = router;
