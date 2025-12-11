7e66ed8ce6ce12e840c1ecbbee9de493c3cfe71e
Sergio <srg.a.neto@gmail.com>
2025-12-11 18:05:11 -0300

    Terminando pequenos ajustes (acho que tá tudo ok)

--- server.js
+++ server.js
@@ -31,6 +31,9 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// servir a pasta uploads em /uploads
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 // Session
 app.use(session({
     secret: "PF$2@@@2$FP",
